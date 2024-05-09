d9f2f67449ebe2ee365d9fd44691cf6b0b7c9035
Abdelrahman shaban <61333407+Abdelrahman7000@users.noreply.github.com>
2024-05-09 17:36:45 +0300

    Time: 41 ms (93.55%), Space: 16.7 MB (27.00%) - LeetHub

--- 1005-maximize-sum-of-array-after-k-negations/1005-maximize-sum-of-array-after-k-negations.py
+++ 1005-maximize-sum-of-array-after-k-negations/1005-maximize-sum-of-array-after-k-negations.py
@@ -2,33 +2,15 @@ class Solution:
     def largestSumAfterKNegations(self, nums: List[int], k: int) -> int:
         
         nums=sorted(nums)
-        def has_neg(lst):
-            neg=0
-            for item in lst:
-                if item < 0:
-                    neg+=1
-            return neg
         
-        negatives=has_neg(nums)
-        if negatives>0:
-            if k>=negatives:
-                for i in range(negatives):
-                    nums[i]*=-1
-                    k-=1
-                nums=sorted(nums)
-                if k%2==0 or k==0:
-                    return sum(nums)
-                else:
-                    nums[0]*=-1
-                    return sum(nums)
-            if k<negatives:
-                for i in range(k):
-                    nums[i]*=-1
-
-                return sum(nums)
-        else:
-            if k%2==0:
-                return sum(nums)
-            else:
-                nums[0]*=-1
-                return sum(nums)
+        for idx in range(len(nums)):
+            if nums[idx]<0 and k>0:
+                nums[idx]*=-1
+                k-=1
+            elif nums[idx]>=0 or k==0:
+                break
+        
+        if k%2 !=0:
+            nums=sorted(nums)
+            nums[0]*=-1
+        return sum(nums)
